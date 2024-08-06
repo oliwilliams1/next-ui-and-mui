3bcd79f60678e392d9b9a0b4eff7b901423068ff
oliwilliams1 <imhungryoliver@gmail.com>
2024-08-07 10:47:47 +1200

    Add three.js

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@
 import { Button as NextUiButton } from "@nextui-org/react";
 import { Button as MuiButton } from '@mui/material';
 
+import SceneCanvas from "./scene.js"
+
 export default function Home() {
   return (
-    <div>
-      <NextUiButton>next ui</NextUiButton>
-      <MuiButton variant="contained">MUI</MuiButton>
-    </div>
-  )
+  <div>
+    <NextUiButton>next ui</NextUiButton>
+    <MuiButton variant="contained">MUI</MuiButton>
+    <SceneCanvas />
+  </div>
+  );
 }
