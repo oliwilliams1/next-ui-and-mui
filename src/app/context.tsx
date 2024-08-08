6ebc4dd1fa568b9231acad7bb60d32e6f87f7159
oliwilliams1 <imhungryoliver@gmail.com>
2024-08-09 00:04:45 +1200

    make a circle

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -41,9 +41,22 @@ export default function Context()
           precision highp float;
           varying vec2 vTexCoord;
 
+          vec4 b = vec4(.1,.1,1.,1.);
+
+          float cubicBezier(float t, vec4 p) {
+            float oneMinusT = 1.0 - t;
+            return oneMinusT * oneMinusT * oneMinusT * p.x +
+                  3.0 * oneMinusT * oneMinusT * t * p.y +
+                  3.0 * oneMinusT * t * t * p.z +
+                  t * t * t * p.w;
+          }
+          
           void main() {
-            //float distanceToCenter = distance(vTexCoord, vec2(0.5, 0.5));
-            gl_FragColor = vec4(vTexCoord, 0.0, 1.0);
+            float d = distance(vTexCoord, vec2(0.5, 0.5));
+            float t = cubicBezier(d, b);
+            t*=2.;
+            float s = (1.0 - t) / 1.0;
+            gl_FragColor = vec4(s, 0.0, 0.0, 1.0);
           }
         `);
 
